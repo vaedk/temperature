9d9f7e95207932b35cf191de3455885c5fa9b41d
vaed <vaed99@gmail.com>
2014-07-28 18:29:27 -0400

    first commit

---
+++ temperature.rb
@@ -0,0 +1,21 @@
+def going_hiking(temp)
+
+
+	if temp >= 50
+
+		puts " #{temp} degrees is perfect for hiking!"
+
+	elsif temp == 23
+
+		puts " #{temp} degrees! Hiking? Are you crazy?"
+
+	elsif temp < 50 
+
+		puts " #{temp} degrees is too cold for hiking!"
+
+	end
+
+end
+
+going_hiking(23)
+
---
+++ todays_temperature.rb
@@ -0,0 +1,12 @@
+todays_temperature = 23
+
+if todays_temperature > 50
+
+	puts "I'm going hiking"
+
+else
+
+	puts "I'm staying in and coding"
+
+end
+
